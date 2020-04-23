1cb2d265c7ed64e96302b7ba0087fd28eece576c
Dante L'Erario <dantelerario@gmail.com>
2020-04-23 15:06:56 +0200

    student js done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
+// js-oggetti-studenti
+//
+// Creare un oggetto che descriva uno studente
+// lo studente avrà  le seguenti proprietà: nome, cognome e età.
+// Stampare attraverso il for..in tutte le proprietà (chiavi e valori).
+
 
 $(document).ready( function() {
 
+  var student = {
+    name: 'Dante',
+    surname: 'LErario',
+    age: 36,
+    class: 'Boolean',
+    classNumber: 12,
+  }
+
+  for (var key in student) {
+    console.log(key + ':', student[key]);
+  }
 
 }); /* END DOCUMENT */
 
